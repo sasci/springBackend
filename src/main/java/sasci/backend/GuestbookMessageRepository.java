06d62a96c9c5d7416a52129c8cabbd5c4a83efe7
Devs <devs@master.com>
2019-07-22 02:36:25 +0300

    Spring Boot with Google Cloud

---
+++ src/main/java/sasci/backend/GuestbookMessageRepository.java
@@ -0,0 +1,8 @@
+package sasci.backend;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface GuestbookMessageRepository extends PagingAndSortingRepository<GuestbookMessage, Long> {
+}
---
+++ src/test/java/sasci/backend/SpringCloudBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package sasci.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
